19a02896247650aca16e488b616dbe7568de3e54
onichandame <zxinmyth@gmail.com>
2021-01-17 03:49:18 +0000

    fix shebang

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # start x11
 docker-entrypoint.sh supervisord &
 # start ibus
@@ -6,4 +8,5 @@ docker-entrypoint.sh supervisord &
 xfconf-query -c xfce4-keyboard-shortcuts -n -p "/commands/custom/<Primary><Alt>t" -s "xfce4-terminal" -t string
 xfconf-query -c xfce4-keyboard-shortcuts -n -p "/commands/custom/Super_L" -s "xfce4-appfinder" -t string
 # frontend keep alive
+"$@"
 wait
