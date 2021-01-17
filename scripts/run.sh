2b177ee9f274df6bff83531e2a56a3c5e14a687a
onichandame <zxinmyth@gmail.com>
2021-01-17 06:21:43 +0000

    change to ubuntu 20

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-# start x11
-docker-entrypoint.sh supervisord &
-# start ibus
-#ibus-daemon &
-# shortcuts
-xfconf-query -c xfce4-keyboard-shortcuts -n -p "/commands/custom/<Primary><Alt>t" -s "xfce4-terminal" -t string
-xfconf-query -c xfce4-keyboard-shortcuts -n -p "/commands/custom/Super_L" -s "xfce4-appfinder" -t string
+function start_desktop(){
+  /root/autostartup.sh &
+}
+
+function start_ibus(){
+  ibus-daemon &
+}
+
+function setup_shortcuts(){
+  xfconf-query -c xfce4-keyboard-shortcuts -n -p "/commands/custom/<Primary><Alt>t" -s "xfce4-terminal" -t string
+  xfconf-query -c xfce4-keyboard-shortcuts -n -p "/commands/custom/Super_L" -s "xfce4-appfinder" -t string
+}
+
+start_desktop
+# wait for desktop env to start
+start_ibus
+#setup_shortcuts
+
 # frontend keep alive
 "$@"
 wait
